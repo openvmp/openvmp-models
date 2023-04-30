f33c69b2cf178e62081acde9cb3d10d7ce2991c0
Roman Kuzmenko <openvmp@proton.me>
2023-04-30 16:05:56 +0000

    Added the first motors to the blueprints

--- robots/don1/link_base.py
+++ robots/don1/link_base.py
@@ -13,6 +13,29 @@ bom = Bom()
 bom.add_part(result, "./parts/custom/don1_board_top")
 bom.add_part(result, "./parts/custom/don1_board_bottom")
 
+for half, dir in [("front", 1), ("rear", -1)]:
+    half_assembly = cq.Assembly()
+
+    channel, channel_name = bom.get_part("./parts/gobilda/structure-u-channel-7")
+    half_assembly.add(
+        channel,
+        name=half + "_" + channel_name,
+        loc=cq.Location((0, -96, 0), (0, 1, 0), 0),
+    )
+
+    motor, motor_name = bom.get_part("./parts/stepperonline/nema17-stepper-brake-72Ncm")
+    for side, side_dir in [("left", -1), ("right", 1)]:
+        half_assembly.add(
+            motor.rotate((0, 0, 0), (0, 0, 1), 90 - side_dir * 90),
+            name=half + "_" + side + "_" + motor_name,
+            loc=cq.Location((-2 * dir, side_dir * 72, 0), (0, 1, 0), 0),
+        )
+
+    result.add(
+        half_assembly,
+        name=half + "_motion",
+        loc=cq.Location((dir * 294, 0, 27), (0, 1, 0), dir * 90),
+    )
 
 if __name__ == "__main__":
     shape = result.toCompound()
